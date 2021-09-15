a7447e047cac5c8421054c52e5d950b9b15040c0
TEAM BLAZE <87557914+TEAM-BLAZ@users.noreply.github.com>
2021-09-15 10:23:08 +0530

    Update start.py

--- handlers/start.py
+++ handlers/start.py
@@ -113,6 +113,8 @@ async def help_(client: Client, message: Message):
         f"""<b>**✯🎼 Hello {message.from_user.mention}** </b>
 
 **In This Menu All Commands Of This Bot Are Available Here..**
+
+
 **POWERED BY•☞ ⏤͟͟͞➖⃟💫🇧ʟᴀᴢᴇ ✘🇲ᴜsɪᴄ ‌‌ﮩ٨ـﮩﮩ٨ـ**""",
         reply_markup=InlineKeyboardMarkup(
             [
