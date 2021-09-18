74119231f7b070d839cac0386363682d99b92ceb
TEAM BLAZE <87557914+TEAM-BLAZ@users.noreply.github.com>
2021-09-18 16:12:47 +0530

    Update pmpermit.py

--- handlers/pmpermit.py
+++ handlers/pmpermit.py
@@ -15,7 +15,7 @@ async def pmPermit(client: USER, message: Message):
             chat_id = message.chat.id
             if chat_id in pchats:
                 return
-            await USER.send_message(
+            await USER.send_message.reply_photo(
                 message.chat.id, 
         photo=f"{BOT_IMG}",
         caption=f"•**☞ HELLO I'M MUSIC ASSISTANT OF** {BOT_NAME}...\n•**☞ POWERED BY**:- @THE_BLAZE_NETWORK\n\n•**⫸** DON'T SPAM MESSAGE.....\n•**⫸** DON'T SEND ME ANYTHING CONFIDENTIAL..\n\n•**☞ JOIN HERE ..  ☟☟☟☟**\n\n•**⫸ MUSIC..** @AS_M_USIC_LOVER\n•**⫸ BLAZE OFFICIAL..** @the_blaze_network\n•**⫸ BLAZE SUPPORT..** @BLAZE_SUPPORT\n•__**⫸ •☞ If You Want I Can Join Your Group. So Send The Link Of Your Group...**__\n\n•**☞ MY BOT..** @BLAZEMUSIC_BOT",
